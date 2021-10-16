828915d98d8a7d18569b176034036d50b25154be
hamed zabihi <hamed0zabihi@gmail.com>
2021-10-16 18:59:26 +0330

    :zap: add pagination with react-paginate

--- src/component/Employee/ArchiveEmployee.jsx
+++ src/component/Employee/ArchiveEmployee.jsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { Container } from "reactstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchEmployees } from "./../../redux/actions/Employee";
+import ReactPaginate from "react-paginate";
+import { Col, Container } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Row } from "reactstrap";
 import EmployeeCard from "./EmployeeCard";
 
 const ArchiveEmployee = () => {
   const Employees = useSelector((state) => state.employees);
+  const dispatch = useDispatch();
+
   const [localStorageMet, setLocalStorageMet] = useState(
     JSON.parse(localStorage.getItem("met"))
   );
+  // current page
+  const [currentPage, setCurrentPage] = useState(0);
 
   // useEffect - update localstorage when check or unchecked checkbox
   useEffect(() => {
@@ -18,6 +24,15 @@ const ArchiveEmployee = () => {
     }
   }, [localStorageMet]);
 
+  // handle page for pagination
+  const handlePageClick = (data) => {
+    let { selected } = data;
+    console.log("selected", selected);
+    // if we want use offset
+    // let offset = Math.ceil(selected * this.props.perPage);
+    dispatch(fetchEmployees(selected + 1));
+    setCurrentPage(selected);
+  };
   //handle checkbox click
   const handleClickCheckBox = (
     event,
@@ -54,7 +69,31 @@ const ArchiveEmployee = () => {
                 {...el}
                 handleClickCheckBox={handleClickCheckBox}
               />
-            ))}
+            ))}{" "}
+      </Row>
+      <Row className="align-items-center  my-5 ">
+        <nav aria-label="Page navigation example ">
+          <ReactPaginate
+            previousLabel="previous"
+            nextLabel="next"
+            breakLabel="..."
+            breakClassName="page-item"
+            pageCount={Employees.total_pages}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            initialPage={currentPage}
+            containerClassName="pagination justify-content-center"
+            activeClassName={"active"}
+            breakLinkClassName="page-item"
+            pageClassName="page-item"
+            previousClassName="page-item"
+            nextClassName="page-item"
+            pageLinkClassName="page-link"
+            previousLinkClassName="page-link"
+            nextLinkClassName="page-link"
+          />
+        </nav>
       </Row>
     </Container>
   );
